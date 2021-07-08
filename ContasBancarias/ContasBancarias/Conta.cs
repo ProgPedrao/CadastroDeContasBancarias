04d36db8e5b43c0218ed2aac93b68d13363a3097
ProgPedrao <pedrohrmartins98@gmail.com>
2021-07-08 02:11:39 -0300

    Primeiro commit do projeto

---
+++ ContasBancarias/ContasBancarias/Conta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContasBancarias
+{
+    class Conta
+    {
+        private const double taxa = 5.00;
+
+
+        public int NumConta { get;  }
+        public string Nome { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int conta, string nome)
+        {
+            NumConta = conta;
+            Nome = nome;
+            this.Saldo = 0;
+        }
+
+        public Conta(int conta, string nome, double saldo) : this(conta, nome)
+        {
+            this.Saldo = saldo;
+        }
+
+        public void Deposito(double valor)
+        {
+            if (valor > 0)
+            {
+                this.Saldo += valor;
+            }
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor > 0)
+            {
+                this.Saldo -= (valor + taxa);
+            }
+        }
+
+        public override string ToString()
+        {
+            return 
+                $"--- Dados da conta ---\n" +
+                $"Numero da conta - {this.NumConta} \n" +
+                $"Nome - {this.Nome} \n" +
+                $"Saldo - {this.Saldo.ToString("f2", CultureInfo.InvariantCulture)} \n";
+        }
+    }
+}
---
+++ ContasBancarias/ContasBancarias/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContasBancarias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double saldo;
+            int numConta;
+            string nome;
+            Conta conta;
+
+            Console.Write("Entre com o número da conta: ");
+            numConta = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Entre com o nome do titular: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Deseja realizar um depósito inicial? [s/n] - ");
+            
+            if (Console.ReadLine().ToLower().StartsWith('s'))
+            {
+                Console.Write("Entre com o valor do depósito: ");
+                saldo = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                conta = new Conta(numConta, nome, saldo);
+            }
+            else
+            {
+                conta = new Conta(numConta, nome);
+            }
+
+            Console.WriteLine(conta.ToString());
+
+            Console.Write("Entre com o valor para o depósito: ");
+            conta.Deposito(Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine(conta.ToString());
+
+            Console.Write("Entre com um valor para saque (R$5,00 de cobrança): ");
+            conta.Sacar(Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine(conta.ToString());
+        }
+    }
+}
